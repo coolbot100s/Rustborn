066eee13c39addf3ead282a1299acadb1f2cd0cb
coolbot100s <76798835+coolbot100s@users.noreply.github.com>
2026-02-03 06:15:44 -0800

    Final touches, new scrap heap, readme update.

--- src/main/java/cool/bot/rustborn/RustbornHeadSpawnSystem.java
+++ src/main/java/cool/bot/rustborn/RustbornHeadSpawnSystem.java
@@ -1,8 +1,5 @@
 package cool.bot.rustborn;
 
-import com.hypixel.hytale.builtin.npccombatactionevaluator.corecomponents.SensorHasHostileTargetMemory;
-import com.hypixel.hytale.builtin.npccombatactionevaluator.evaluator.CombatActionEvaluator;
-import com.hypixel.hytale.builtin.npccombatactionevaluator.memory.TargetMemory;
 import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.ComponentType;
 import com.hypixel.hytale.component.Ref;
@@ -12,14 +9,10 @@ import com.hypixel.hytale.component.system.RefChangeSystem;
 import com.hypixel.hytale.server.core.modules.entity.component.PersistentModel;
 import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
 import com.hypixel.hytale.server.core.modules.entity.damage.DeathComponent;
-import com.hypixel.hytale.server.core.modules.entity.player.PlayerSkinComponent;
-import com.hypixel.hytale.server.core.modules.entitystats.EntityStatMap;
 import com.hypixel.hytale.server.core.universe.world.World;
 import com.hypixel.hytale.server.core.universe.world.npc.INonPlayerCharacter;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
-import com.hypixel.hytale.server.flock.FlockMembership;
 import com.hypixel.hytale.server.npc.NPCPlugin;
-import com.hypixel.hytale.server.npc.role.Role;
 import it.unimi.dsi.fastutil.Pair;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -50,42 +43,37 @@ public class RustbornHeadSpawnSystem extends RefChangeSystem<EntityStore, DeathC
             modelName = persistentModel.getModelReference().getModelAssetId();
         }
 
-        // Filter to Rustborn Skeletons that aren't floating heads.
-        if (modelName.contains("Skeleton_Rustborn_") && !modelName.contains("Skeleton_Rustborn_Head")) {
+        // Filter to Rustborn Skeletons that aren't floating heads or soulless (summoned) Rustborn Knights.
+        if (!modelName.contains("Rustborn_") || modelName.contains("Skeleton_Rustborn_Head") || modelName.contains("Risen_Rustborn_Knight")) {
+            return;
+        }
 
-            // Get the transform from the dying entity so we can spawn the head at the same location.
-            TransformComponent transform = entityStore.getComponent(ref, TransformComponent.getComponentType());
-            if (transform == null) {
-                return;
-            }
+        // Get the transform from the dying entity so we can spawn the head at the same location.
+        TransformComponent transform = entityStore.getComponent(ref, TransformComponent.getComponentType());
+        if (transform == null) {
+            return;
+        }
 
-            // Use flock membership to determine if this is a summoned Rustborn.
-            World world = entityStore.getExternalData().getWorld();
-            FlockMembership flockMembership;
-            if (entityStore.getComponent(ref, FlockMembership.getComponentType()) != null) {
-                flockMembership = entityStore.getComponent(ref, FlockMembership.getComponentType());
-            } else {
-                flockMembership = null;
-            }
+        // pick between crawler or head, or use head if this is already a crawler.
+        String npcType;
+        if (modelName.contains("Rustborn_Crawler")) {
+            npcType = "Skeleton_Rustborn_head";
+        } else {
+            npcType = Math.random() < 0.35 ? "Rustborn_Crawler" : "Skeleton_Rustborn_Head";
+        }
 
-            //LOGGER.atInfo().log("Archetype for %s: %s", ref, entityStore.getArchetype(ref));
-
-            // Spawn the NPC on the world thread
-            world.execute(() -> {
-                // Don't spawn a head if the Rustborn is a summoned one.
-                if (flockMembership == null) {
-                    // Head
-                    Pair<Ref<EntityStore>, INonPlayerCharacter> head = NPCPlugin.get().spawnNPC(
+        // Spawn the NPC on the world thread
+        World world = entityStore.getExternalData().getWorld();
+        world.execute(() -> {
+                    Pair<Ref<EntityStore>, INonPlayerCharacter> scrapHeap = NPCPlugin.get().spawnNPC(
                             entityStore,
-                            "Skeleton_Rustborn_Head",
+                            npcType,
                             null,
                             transform.getPosition(),
                             transform.getRotation()
                     );
                 }
-            });
-        }
-
+        );
     }
 
     @Override
